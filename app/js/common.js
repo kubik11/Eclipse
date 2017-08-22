255040e15209ea51adf30ba67d9c6ccef328de5b
Herr Gunter <kubyshkin1990@mail.ru>
2017-08-22 23:09:26 +0300

    22.08.2017 night

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
+
+	function eqH(){
+		
+		$('.offers-inner p').each(function(elem){
+			var val = 0;
+			var current = $(this).height();
+			//alert(current);
+			if (val<current){
+				val = current;
+			}
+			});
+		$('.offers-inner p').each(function(i){
+				$(this).css('height', val + 'px');
+			});
+		
+	}
+	eqH();
 	// Custom JS
 
 });
